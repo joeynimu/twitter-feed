6944cdabbf148909510d5a1d58433e3774d54757
Joe Ng'ethe <joeynimu@gmail.com>
2015-11-03 13:59:18 +0300

    added the config file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'H1jnUT2AlhPbShQMA43RLSFog',
+		consumer_secret: 'ok4yJ9AtwKAlYiA5PEAOewHAEYPgriy6rfBibuuLDtzIMTp1U4',
+		access_token: '150192980-JZqxPn4guhmyOxpVI7C5JRmWLDOtgkf6xvZTFWQP',
+		access_token_secret: 'sZC6yeosPru2rygY4dlx1YvOY6r4i20EubP2HGc6h5vfi'
+	}
+
+};
